b395585c6e4c574dbbe945d0b0f7b2188825a916
hamdyjs <hamdyjs@gmail.com>
2017-08-21 19:30:42 +0200

    Implement the server code

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var jwt = require('jsonwebtoken');
+var api = require('./api');
+
+var app = express();
+
+app.use(morgan());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.listen(3001, function(err) {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    console.log('Server started listening on port 3001');
+});
+
+app.get('/', function(req, res) {
+    res.end('Hello, World!');
+});
+
+app.route('/auth').post(api.generateToken);
+app.route('/room').post(api.createRoom);
