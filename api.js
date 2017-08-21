4b5fbb4f51259b8bde433bcf15eda725fef4f5fe
hamdyjs <hamdyjs@gmail.com>
2017-08-21 20:18:44 +0200

    Implement creating rooms

--- api.js
+++ api.js
@@ -9,17 +9,35 @@ var tokens = {};
 
 api.generateToken = function(req, res) {
     var name = req.body.name;
-    var token = jwt.sign(name, config.secret, {
-        expiresIn: 3600
+    var token = jwt.sign({name: name}, config.secret, {
+        expiresIn: Math.floor(Date.now() / 1000) + (60 * 60)
     });
 
     console.log('Generated token: ' + token);
     tokens[token] = {rooms: 0, lastTimestamp: Date.now()};
-    res.json(token);
+    res.status(200).json(token);
 };
 
 api.createRoom = function(req, res) {
-    
+    var token = req.body.token;
+    if (!token) {
+        res.status(403).json();
+        return;
+    }
+
+    jwt.verify(token, config.secret, function(err, decoded) {
+        if (err) {
+            res.status(403).json();
+            return;
+        }
+
+        console.log(decoded);
+        tokens[token].rooms = 1;
+        tokens[token].lastTimestamp = Date.now();
+        
+        // Generate room key
+        res.status(200).json();
+    });
 };
 
 module.exports = api;
