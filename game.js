a445422c2ebae267ae0b2659b4f693f81c7bde2e
hamdyjs <hamdyjs@gmail.com>
2017-08-22 15:34:37 +0200

    Add the skeleton for the game module

---
+++ game.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var game = {};
+
+game.init = function(io) {
+    
+};
+
+game.createRoom = function(token) {
+
+};
+
+game.doesTokenOwnRoom = function(token) {
+    
+}
+
+module.exports = game;
